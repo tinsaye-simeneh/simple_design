cafb8a7036e9e8e1da8d1dec40319d9d4109deb3
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-07-08 00:07:52 +0300

    created react app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function Simple(){
+  return(
+    <>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Simple/>
+);
+
