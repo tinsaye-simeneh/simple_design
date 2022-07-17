08ad89b91d64d8b1c1735102650ffcb6553678e9
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-07-17 20:45:58 +0300

    navbar link created

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,20 +4,30 @@ const navLeftElements = ["Home", "Source Code"];
 const navRightElements = ["Login", "Signup"];
 
 function Navbar() {
+    function navElementController({arrName}) {
+        arrName.map((element, index) => {
+            console.log(element);
+            return (
+              <a key={index} className="p-5" href={element} style={{textDecoration:'none', color:'black'}}>
+                {element}
+              </a>
+            );
+          })
+    }
   return (
     <>
       <div
-        className="container mt-2 text-center"
-        style={{ border: "2px solid #103037" }}
+        className="container mt-3 text-center"
       >
         <div className="row">
-          <div className="col-sm-4" style={{ backgroundColor: "yellow" }}>
-            <ul className="col-6" style={{ listStyleType: "none" }}>
-              <li>{navLeftElements[0]}</li>
-            </ul>
+          <div className="col-sm-4 mt-4">
+            {navElementController({arrName: navLeftElements}) }
           </div>
-          <div className="col-sm-4" style={{ backgroundColor: "yellow" }}>
+          <div className="col-sm-4">
             <img src={logo} alt="logo" />
+          </div>
+          <div className="col-sm-4 mt-4">
+          {navElementController({arrName: navRightElements})}
           </div>
         </div>
       </div>
