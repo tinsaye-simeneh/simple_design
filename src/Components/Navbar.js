9b6c66b03893447f194864662f13e960e44d15a2
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-07-17 19:54:23 +0300

    created a nav container div

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,16 @@
 import React from "react";
+import logo from "../assets/images/Logo/Logo.png";
+const navLeftElements = ["Home", "Source Code"];
+const navRightElements = ["Login", "Signup"];
 
 function Navbar() {
-  return(
-  <>
-  <h2>Test</h2>
-  </>
-  )
+  return (
+    <>
+      <div className="container" style={{backgroundColor:'yellow'}}>
+        <img src={logo} alt="logo" />
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './Components/pages/Home.js';
+import 'bootstrap' 
 
 export default function Simple(){
   return(
