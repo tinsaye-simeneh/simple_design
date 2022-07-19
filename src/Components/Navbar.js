570e998f5f68657fc374a7214940704cb2e9a4e5
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-07-19 20:08:36 +0300

    Minor update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,44 +1,45 @@
 import React from "react";
 import "../Components/navBar.css";
 import logo from "../assets/images/Logo/Logo.png";
-const navLeftElements = ["Home", "SourceCode"];
-const navRightElements = ["Login", "Signup"];
+const navElements = ["Home", "SourceCode", "Login", "Signup"];
 
 function Navbar() {
   return (
     <>
-      <div className="container mt-4 text-center main">
-        <div className="row align-items-center" style={{display: { sm: "none", md: "none" }}}>
-          <div className="col-sm-4">
-            {navLeftElements.map((element, index) => {
-              return (
-                <>
-                  <a key={index} className="linkStyle px-3 mx-5" href={element}>
-                    {element}
-                  </a>
-                </>
-              );
-            })}
-          </div>
-          <div className="col-sm-4 ps-5">
-            <img src={logo} alt="logo" />
-          </div>
-          <div className="col-sm-4">
-            {navRightElements.map((element, index) => {
-              return (
-                <>
-                  <a key={index} className="linkStyle pe-4 mx-5" href={element}>
-                    {element}
-                  </a>
-                </>
-              );
-            })}
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container mt-3">
+        <div className="col-sm-8">
+              <img src={logo} alt="logo" />
+            </div>
+          <button
+            type="button"
+            className="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            onClick={console.log("clicked")}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+
+            <div className="col-sm-4">
+              {navElements.map((element, index) => {
+                return (
+                  <>
+                    <a
+                      key={index}
+                      className="linkStyle px-3 mx-2"
+                      href={element}
+                    >
+                      {element}
+                    </a>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
-        <div className="row mt-3">
-            <hr/>
-        </div>
-      </div>
+      </nav>
     </>
   );
 }
